1f75b92d7dd60d67cacd0cb1d5f03536b6a54ff8
Dylan Pogue <POGUEDJ@ETSU.EDU>
2022-09-22 16:37:03 -0400

    Add project files.

---
+++ File IO/FileRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    internal class FileRoot
+    {
+        public static string projectRoot = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+    }
+}
